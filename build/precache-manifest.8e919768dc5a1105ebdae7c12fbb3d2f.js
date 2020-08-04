900825ce249147c1ed6c7403562bf325fc8da166
Mohamed Ibrahim Mohamed Yossef <49948244+MohamedIbrahim13@users.noreply.github.com>
2020-08-04 18:25:09 +0200

    Add files via upload

---
+++ build/precache-manifest.8e919768dc5a1105ebdae7c12fbb3d2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be33f708833646ee64771b73c9b033c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b44cf2028677a1299767",
+    "url": "/static/css/main.da62ca87.chunk.css"
+  },
+  {
+    "revision": "b5cfed3efb73a820058c",
+    "url": "/static/js/2.e7132ea9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7132ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44cf2028677a1299767",
+    "url": "/static/js/main.543e6c1c.chunk.js"
+  },
+  {
+    "revision": "6bb6b1442e19a2f89113",
+    "url": "/static/js/runtime-main.9c48c91b.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
